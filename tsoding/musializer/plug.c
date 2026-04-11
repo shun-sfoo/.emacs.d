6f98fb3efe7b4b4904df947cec37732f59c7dfdb
shun-sfoo <ganymede0915@gmail.com>
2026-04-11 23:46:30 +0800

    tsoding:musializer part2 start

---
+++ tsoding/musializer/plug.c
@@ -0,0 +1,11 @@
+#include "plug.h"
+
+#include <stdio.h>
+
+void plug_init() {
+  printf("plug init\n");
+}
+
+void plug_update() {
+  printf("plug update\n");
+}
---
+++ tsoding/musializer/plug.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void plug_init();
+void plug_update();
+
