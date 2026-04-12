3daf08b8c3fa52e7a96ab09407a7f26ca3ae7138
shun-sfoo <ganymede0915@gmail.com>
2026-04-12 16:28:17 +0800

    tsoding:musializer part2.1 hot reload

--- tsoding/musializer/main.c
+++ tsoding/musializer/main.c
@@ -1,112 +1,95 @@
+#include "plug.h"
 #include <assert.h>
 #include <complex.h>
-#include <math.h>
+#include <dlfcn.h>
 #include <raylib.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define N 256
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
-typedef struct {
-  float left;
-  float right;
-} Frame;
-
-float in[N];
-float complex out[N];
-float max_amp;
-
-float amp(float complex z) {
-  float a = fabsf(crealf(z));
-  float b = fabsf(cimagf(z));
-  if (a < b)
-    return b;
-  return a;
+char *shift_args(int *argc, char ***argv) {
+  assert(*argc > 0);
+  char *result = (**argv);
+  (*argv) += 1;
+  (*argc) -= 1;
+  return result;
 }
 
-void fft(float in[], size_t stride, float complex out[], size_t n) {
-  assert(n > 0);
+plug_hello_t plug_hello = NULL;
+plug_init_t plug_init = NULL;
+plug_update_t plug_update = NULL;
+Plug plug = {0};
 
-  if (n == 1) {
-    out[0] = in[0];
-    return;
+const char *libplug_file_name = "./libplug.so";
+void *libplug = NULL;
+
+bool reload_libplug() {
+  if (libplug != NULL) {
+    dlclose(libplug);
   }
 
-  fft(in, stride * 2, out, n / 2);
-  fft(in + stride, stride * 2, out + n / 2, n / 2);
-
-  for (size_t k = 0; k < n / 2; ++k) {
-    float t = (float)k / n;
-    float complex v = cexp(-2 * I * PI * t) * out[k + n / 2];
-    float complex e = out[k];
-    out[k] = e + v;
-    out[k + n / 2] = e - v;
+  libplug = dlopen(libplug_file_name, RTLD_NOW);
+  if (libplug == NULL) {
+    fprintf(stderr, "ERROR: could not load %s: %s\n", libplug_file_name,
+            dlerror());
+    return false;
   }
+
+  plug_hello = dlsym(libplug, "plug_hello");
+  if (plug_hello == NULL) {
+    fprintf(stderr, "Error: could not find plug_hello symbol in %s: %s\n",
+            libplug_file_name, dlerror());
+    return false;
+  }
+
+  plug_init = dlsym(libplug, "plug_init");
+  if (plug_init == NULL) {
+    fprintf(stderr, "Error: could not find plug_init symbol in %s: %s\n",
+            libplug_file_name, dlerror());
+    return false;
+  }
+
+  plug_update = dlsym(libplug, "plug_update");
+  if (plug_update == NULL) {
+    fprintf(stderr, "Error: could not find plug_update symbol in %s: %s\n",
+            libplug_file_name, dlerror());
+    return false;
+  }
+
+  return true;
 }
 
-void callback(void *bufferData, unsigned int frames) {
-  if (frames < N)
-    return;
+int main(int argc, char **argv) {
+  if (!reload_libplug()) {
+    return 1;
+  };
 
-  Frame *fs = bufferData;
-  for (size_t i = 0; i < N; ++i) {
-    in[i] = fs[i].left;
+  const char *program = shift_args(&argc, &argv);
+  printf("grogram %s\n", program);
+
+  if (argc == 0) {
+    fprintf(stderr, "Usage: %s <input>\n", program);
+    fprintf(stderr, "ERROR: no input file is provided\n");
+    return 1;
   }
 
-  fft(in, 1, out, N);
+  const char *file_path = shift_args(&argc, &argv);
 
-  max_amp = 0.0f;
-  for (size_t i = 0; i < N; ++i) {
-    float a = amp(out[i]);
-    if (max_amp < a)
-      max_amp = a;
-  }
-}
-
-int main(void) {
   InitWindow(800, 600, "Musializer");
   SetTargetFPS(60);
 
   InitAudioDevice();
-  Music music = LoadMusicStream("complex_spectrum.ogg");
-
-  printf("music.frameCount = %u\n", music.frameCount);
-  printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
-  printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
-  printf("music.stream.channels = %u\n", music.stream.channels);
-  assert(music.stream.sampleSize == 16);
-  assert(music.stream.channels == 2);
-
-  PlayMusicStream(music);
-  SetMusicVolume(music, 0.5f);
-  AttachAudioStreamProcessor(music.stream, callback);
+  plug_init(&plug, file_path);
 
   while (!WindowShouldClose()) {
-    UpdateMusicStream(music);
-
-    if (IsKeyPressed(KEY_SPACE)) {
-      if (IsMusicStreamPlaying(music)) {
-        PauseMusicStream(music);
-      } else {
-        ResumeMusicStream(music);
+    if (IsKeyPressed(KEY_R)) {
+      if (!reload_libplug()) {
+        return 1;
       }
     }
-
-    int w = GetRenderWidth();
-    int h = GetRenderHeight();
-
-    BeginDrawing();
-    ClearBackground(CLITERAL(Color){0x18, 0x18, 0x18, 0xFF});
-    float cell_wdith = (float)w / N;
-    for (size_t i = 0; i < N; ++i) {
-      float t = amp(out[i]) / max_amp;
-      DrawRectangle(i * cell_wdith, h / 2 - h / 2 * t, cell_wdith, h / 2 * t,
-                    RED);
-    }
-
-    EndDrawing();
+    plug_update(&plug);
   }
 }
--- tsoding/musializer/plug.h
+++ tsoding/musializer/plug.h
@@ -1,5 +1,11 @@
 #pragma once
+#include <complex.h>
+#include <raylib.h>
 
-void plug_init();
-void plug_update();
+typedef struct {
+  Music music;
+} Plug;
 
+typedef void (*plug_hello_t)(void);
+typedef void (*plug_init_t)(Plug *plug, const char *file_path);
+typedef void (*plug_update_t)(Plug *plug);
